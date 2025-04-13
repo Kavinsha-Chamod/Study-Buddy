42748d18b641a17e6f894614c9650f60dba67567
Kavinsha Chamod <kavinshachamod2001@gmail.com>
2025-04-13 18:18:40 +0530

    Initial Commit

---
+++ Study Buddy/Study_BuddyApp.swift
@@ -0,0 +1,20 @@
+//
+//  Study_BuddyApp.swift
+//  Study Buddy
+//
+//  Created by Kavinsha Chamod on 2025-04-13.
+//
+
+import SwiftUI
+
+@main
+struct Study_BuddyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
